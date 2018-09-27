b35342968cec12b391194ed04c5a3b473047ac93
Anastasiya Shadrina <a.shadrina5@mail.ru>
2018-09-27 19:32:34 +0700

    Reshuffle

--- lab-1/src/main/kotlin/Main.kt
+++ lab-1/src/main/kotlin/Main.kt
@@ -3,10 +3,11 @@ import java.net.DatagramPacket
 import java.net.DatagramSocket
 import java.net.InetAddress
 import java.net.MulticastSocket
+import java.util.Random
 
 const val TTL = 10
-const val MULTICAST_PORT = 60000
-const val MY_PORT = 51111
+const val MULTICAST_PORT = 24242
+const val MY_PORT = 50505
 
 data class MulticastGroupMemberInfo(val address: InetAddress, var timeRemaining: Int = TTL)
 
@@ -25,7 +26,7 @@ class ServerRunnable(
                         MULTICAST_PORT
                 )
                 datagramSocket.send(msgPacket)
-                Thread.sleep(1000L)
+                Thread.sleep(20000)
             }
         } catch (ex: IOException) {
             ex.printStackTrace()
@@ -42,6 +43,7 @@ class ClientRunnable(private val multicastSocket: MulticastSocket) : Runnable {
             val buf = ByteArray(1024)
             val packet = DatagramPacket(buf, buf.size)
             multicastSocket.receive(packet)
+            println("Got!")
             if (!others.any { it.address == packet.address }) {
                 println("New member ${packet.address}:${packet.port}")
                 others.add(MulticastGroupMemberInfo(packet.address))
@@ -51,25 +53,38 @@ class ClientRunnable(private val multicastSocket: MulticastSocket) : Runnable {
                 others.forEach { if (it.address == packet.address) it.timeRemaining = TTL }
             }
             others.forEach { it.timeRemaining-- }
+
+            if (others.any { it.timeRemaining == 0 }) {
+                others.forEach { if (it.timeRemaining == 0) println(it.address.toString() + " is leaving!") }
+                others.filter{ it.timeRemaining != 0 }.forEach { println(it.address) }
+                println()
+            }
             others.removeIf { it.timeRemaining == 0 }
         }
     }
 }
 
 fun main(args: Array<String>) {
-    val myAddress = InetAddress.getByName(args.getOrNull(0))
+    val random = Random()
+    fun rand(from: Int, to: Int) : Int {
+        return random.nextInt(to - from) + from
+    }
+    val number = rand(1, 255)
+    val myAddress = InetAddress.getByName("127.0.0.$number")
     if (!myAddress.isLoopbackAddress) {
         println("Not a loopback address")
         return
     }
-    val multicastAddress = InetAddress.getByName(args.getOrNull(1))
+    val multicastAddress = InetAddress.getByName(args.getOrNull(0))
     if (!multicastAddress.isMulticastAddress) {
         println("Not a multicast address")
         return
     }
 
+
     val datagramSocket = DatagramSocket(MY_PORT, myAddress)
     val multicastSocket = MulticastSocket(MULTICAST_PORT)
+    println(multicastSocket.port)
     multicastSocket.joinGroup(multicastAddress)
 
     val clientThread = Thread(ClientRunnable(multicastSocket))
